ae845d3e6e5294439e4933e8c7d6fb5b63263c90
slave01 <slave01@localhost.localdomain>
2021-09-11 23:36:05 -0400

    Add implementation for memcopy with overlap

--- c_practice/memcopy.c
+++ c_practice/memcopy.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 
-void * memcopy2(void * dest, void * src, size_t n)
+void * memcpy2(void * dest, void * src, size_t n)
 {
     // Cast input to uint8_t*ptr
     char * destPtr = (char*)dest;
@@ -16,8 +16,21 @@ void * memcopy2(void * dest, void * src, size_t n)
     return src;
 }
 
-
-
+void * memcpy_overlap(void * dest, void * src, size_t n)
+{
+    char temp[1024]; // <-- make some huge buffer
+    char * destPtr = (char*)dest;
+    char * srcPtr = (char*)src;
+    for (int i =0; i <n; i++)
+    {
+        temp[i] = srcPtr[i];
+    }
+    for (int i =0; i <n; i++)
+    {
+        destPtr[i] = temp[i];
+    }
+    return dest;
+}
 
 int main(void)
 {
@@ -26,10 +39,19 @@ int main(void)
 
     // copy first 4 elements of arr2 into arr1
     unsigned int * retPtr;
-    retPtr = memcopy2((void*)&arr1, (void*)&arr2, 4*(sizeof(unsigned int)));
+
+    printf("Source array [");
+    int i = 0;
+    for (i = 0; i < 10-1; i++)
+    {
+        printf(" %lu,", arr1[i]);
+    }
+    printf( " %lu ]\n", arr1[i]);
+
+    retPtr = (unsigned int*)memcpy_overlap((void*)&arr1[1], (void*)&arr1[0], 3*(sizeof(unsigned int)));
 
     printf("Result array [");
-    int i = 0;
+    i = 0;
     for (i = 0; i < 10-1; i++)
     {
         printf(" %lu,", arr1[i]);
